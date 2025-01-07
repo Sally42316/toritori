20e11ca7bd67082adec776b6d142d81cd95ad49a
EC2 Default User <ec2-user@ip-172-31-35-214.eu-north-1.compute.internal>
2025-01-07 05:04:38 +0000

    0107

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,30 @@
 class UsersController < ApplicationController
   def show
+    @user = current_user  # ログインしているユーザーを取得
   end
 
   def edit
+    @user = current_user  # ログインしているユーザーを取得
   end
 
   def update
+    @user = current_user  # ログインしているユーザーを取得
+    if @user.update(user_params)  # ユーザー情報を更新
+      redirect_to @user, notice: 'Profile updated successfully'
+    else
+      render :edit  # 更新に失敗した場合は再度編集フォームを表示
+    end
   end
 
   def destroy
+    @user = current_user
+    @user.destroy
+    redirect_to root_path, notice: 'Your account has been deleted'
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :bio)  # 編集可能な属性を指定
   end
 end
