8a391c360ddff132d99f359fbed90aa7609c73a1
EC2 Default User <ec2-user@ip-172-31-35-214.eu-north-1.compute.internal>
2025-01-11 05:58:06 +0000

    管理者権限ログインのエラー突破7

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,5 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
-    @comments = Comment.all
-    redirect_to admin_comments_path
   end
 end
--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
+
+  # 管理者がログインした後の遷移先を指定
+  def after_sign_in_path_for(resource)
+    admin_root_path  # /admin/homes/top にリダイレクトされます
+  end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
