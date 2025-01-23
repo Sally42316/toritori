294abcb55b90751df7ce3ae37690f483552861f9
EC2 Default User <ec2-user@ip-172-31-35-214.eu-north-1.compute.internal>
2025-01-23 03:43:59 +0000

    いいねをハートマーク化した

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,14 +6,14 @@ class LikesController < ApplicationController
     unless @post.liked_by?(current_user)
       @post.likes.create(user: current_user)
     end
-    redirect_to @post
+    redirect_to root_path, notice: 'いいねしました！'
   end
 
   # いいねを削除
   def destroy
     like = @post.likes.find_by(user: current_user)
     like.destroy if like
-    redirect_to @post
+    redirect_to root_path, notice: 'いいねを解除しました！'
   end
 
   private
