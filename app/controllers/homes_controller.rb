60b2f9aa4ee3859832befd84207e21e4a885ae9b
EC2 Default User <ec2-user@ip-172-31-35-214.eu-north-1.compute.internal>
2025-01-27 05:09:50 +0000

    解決

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,15 @@
 class HomesController < ApplicationController
   def top
     # ログインユーザーの投稿と他ユーザーの投稿を時系列順に取得
-    @posts = Post.order(created_at: :desc).includes(:user) # 投稿を新しい順に取得
+    # @posts = Post.order(created_at: :desc).includes(:user) # 投稿を新しい順に取得
+    # 「いいね数順」に並べ替えを実現
+    if params[:sort] == 'likes'
+      # いいね数順に並べ替え（left_joinsを使っていいね数0の投稿も含める）
+      @posts = Post.left_joins(:likes).group(:id).order('COUNT(likes.id) DESC')
+    else
+      # デフォルトは投稿日時順
+      @posts = Post.order(created_at: :desc)
+    end
   end
 
   def about
