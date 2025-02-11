74a792fdc7b574e674b43a1da7974b05861fd1c0
EC2 Default User <ec2-user@ip-172-31-35-214.eu-north-1.compute.internal>
2025-02-11 08:25:07 +0000

    エラー解決済み

--- db/migrate/20250208100019_add_group_apply_notification_type_to_notifications.rb
+++
@@ -1,5 +0,0 @@
-class AddGroupApplyNotificationTypeToNotifications < ActiveRecord::Migration[6.1]
-  def change
-    add_column :notifications, :notification_type, :integer, default: 0, null: false
-  end
-end
