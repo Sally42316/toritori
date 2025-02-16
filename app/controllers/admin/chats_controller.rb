9d1c3de714195b1c9f5c59de0e6f998254bc51b3
EC2 Default User <ec2-user@ip-172-31-35-214.eu-north-1.compute.internal>
2025-02-16 10:50:41 +0000

    構文修正

--- app/controllers/admin/chats_controller.rb
+++ app/controllers/admin/chats_controller.rb
@@ -8,27 +8,27 @@ class Admin::ChatsController < Admin::ApplicationController
       @chat = Chat.new
     end
 
-    # POST /admin/groups/:group_id/chats
-    def create
-      @chat = @group.chats.new(chat_params)
-      @chat.user = current_user
-      if @chat.save
-        redirect_to admin_group_chats_path(@group), notice: 'メッセージが送信されました。'
-      else
-        @chats = @group.chats.includes(:user)
-        render :index, status: :unprocessable_entity
-      end
-    end
+    # # POST /admin/groups/:group_id/chats
+    # def create
+    #   @chat = @group.chats.new(chat_params)
+    #   @chat.user = current_user
+    #   if @chat.save
+    #     redirect_to admin_group_chats_path(@group), notice: 'メッセージが送信されました。'
+    #   else
+    #     @chats = @group.chats.includes(:user)
+    #     render :index, status: :unprocessable_entity
+    #   end
+    # end
 
-    # DELETE /admin/groups/:group_id/chats/:id
-    def destroy
-        @chat = @group.chats.find(params[:id])
-        if @chat.destroy
-          redirect_to admin_group_chats_path(@group), notice: 'メッセージを削除しました。'
-        else
-          redirect_to admin_group_chats_path(@group), alert: 'メッセージの削除に失敗しました。'
-        end
-    end
+    # # DELETE /admin/groups/:group_id/chats/:id
+    # def destroy
+    #     @chat = @group.chats.find(params[:id])
+    #     if @chat.destroy
+    #       redirect_to admin_group_chats_path(@group), notice: 'メッセージを削除しました。'
+    #     else
+    #       redirect_to admin_group_chats_path(@group), alert: 'メッセージの削除に失敗しました。'
+    #     end
+    # end
 
     
 
